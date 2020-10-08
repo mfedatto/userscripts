8cbd9bfb70c622fc322b59b2716617e4ede23aff
Maurício Fedatto <mfedatto@gmail.com>
2020-10-08 12:36:01 -0300

    Update block_do_mau.user.js

--- block_do_mau.user.js
+++ block_do_mau.user.js
@@ -1,8 +1,18 @@
 // ==UserScript==
 // @name        Block do Mau
-// @namespace   com.github.mfedatto.userscripts.block_do_mau
+// @namespace   https://github.com/mfedatto/userscripts/block_do_mau.user.js
+// @version     1.0.0
+// @date        2020-10-08
+// @author      Maurício Fedatto
+/*// @description Youtube Downloader: all in one script to get Vimeo, Facebook, Dailymotion videos for free*/
+// @icon        https://sf-helper.com/img/icon_16.png
+// @icon64      https://sf-helper.com/img/icon_64.png
+// @homepage    https://github.com/mfedatto/userscripts
+// @updateURL   https://github.com/mfedatto/userscripts/raw/main/block_do_mau.user.js
+// @downloadURL https://github.com/mfedatto/userscripts/raw/main/block_do_mau.user.js
 // @include     https://www.globo.com/*
 // @include     https://www.uol.com.br/*
+// @run-at      document-end
 // @grant       none
 // @require     https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
