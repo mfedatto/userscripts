5aff88731fb8ce14c223d0bb7ba9703069ed9ac1
Maurício Fedatto <mfedatto@gmail.com>
2020-10-08 09:57:57 -0300

    Create block_do_mau.user.js

---
+++ block_do_mau.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name        Block do Mau
+// @namespace   com.github.mfedatto.userscripts.block_do_mau
+// @include     https://www.globo.com/*
+// @include     https://www.uol.com.br/*
+// @grant       none
+// @require     https://code.jquery.com/jquery-3.2.1.min.js
+// ==/UserScript==
+
+new (function() {
+  const forbiddenLinkKeywordsList = [
+    '/futebol/',
+    '/blog-do-pvc/',
+    '/famosos/',
+    '/celebridades/',
+    '/programas/',
+    '/novelas/',
+    '/quem-news/',
+    '//revistaquem.',
+    '/moda/',
+    '/telinha/',
+    '/cartola-fc/',
+    '/futebol-no-japao/',
+    '//cartolafc.',
+    '/musa/',
+    '/ela/gente/',
+    '/tv-e-lazer/',
+    '//globoplay.',
+    '//globosatplay.',
+    '//casavogue.',
+    '//kogut.',
+    '//gq.',
+    '//vogue.',
+    '//extra.',
+    '//revistamonet.',
+    '/esporte/futebol-muleke',
+    '//tvefamosos.',
+    '//ninalemos.',
+    '//soltos.',
+    '/brasil-mundial-fc/',
+    '/pop-arte/'
+  ];
+  
+  $('a').each((iLink, eLink) => {
+    const href = $(eLink).attr('href');
+    
+    if (href !== undefined) {
+      $(forbiddenLinkKeywordsList).each((iForbiddenLinkKeyword, eForbiddenLinkKeyword) => {
+        if (href.toLowerCase().indexOf(eForbiddenLinkKeyword) > -1) {
+          $(eLink).css('visibility', 'hidden');
+        }
+      });
+    }
+  });
+})();
