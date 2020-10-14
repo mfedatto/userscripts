6a86bcd23cca550c04f5fc356582bd2ffa6c4665
Maurício Fedatto <mfedatto@gmail.com>
2020-10-14 11:40:32 -0300

    Update block_do_mau.user.js

--- block_do_mau.user.js
+++ block_do_mau.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Block do Mau
 // @namespace   https://github.com/mfedatto/userscripts/block_do_mau.user.js
-// @version     1.1.17
+// @version     1.1.18
 // @date        2020-10-14
 // @author      Maurício Fedatto
 // @description Block do Mau: script do Maurício Fedatto para bloquear conteúdo indesejado
@@ -83,7 +83,7 @@ new (function() {
     }
   });
   
-  if (document.location.href.toLowerCase() === 'https://www.uol.com.br/') {
+  if (document.location.href.toLowerCase().indexOf('https://www.uol.com.br/') === 0) {
     const adsSelectors = [
       { selector: 'div.full.moduloBanner.banner', action: (selector) => $(selector).remove() },
       { selector: 'div.banner-1x1-hibrido-video-container.labeled.label-publicidade', action: (selector) => $(selector).css('visibility', 'hidden') },
@@ -106,7 +106,7 @@ new (function() {
       });
   }
   
-  if (document.location.href.toLowerCase() === 'https://www.ig.com.br/') {
+  if (document.location.href.toLowerCase().indexOf('https://www.ig.com.br/') === 0) {
     const adsSelectors = [
       { selector: 'div.fc-ab-root', action: (selector) => { $(selector).remove(); $(document.body).css('overflow', 'auto') } }
     ];
